c4def71342326fe9b1c911dbe0a4e4fb23dbb037
David Hernandez <54561496+David-Hdez@users.noreply.github.com>
2022-05-21 15:46:52 -0500

    Formulario de registro de usuarios

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,7 +1,22 @@
 <template>
     <BasicLayout>
-        <h2>Estamos en la Register</h2>
-        <router-link to="/iniciar-sesion">Iniciar sesión</router-link>
+        <div class="register">
+            <h2>Registro de usuario</h2>
+            <form class="ui form">
+                <div class="field">
+                    <input type="text" placeholder="Nombre de usuario">
+                </div>
+                <div class="field">
+                    <input type="email" placeholder="Correo electrónico">
+                </div>
+                <div class="field">
+                    <input type="password" placeholder="Contraseña">
+                </div>
+                <button type="submit" class="ui button fluid primary">Crear usuario</button>
+            </form>
+
+            <router-link to="/iniciar-sesion">Iniciar sesión</router-link>
+        </div>
     </BasicLayout>
 </template>
 
@@ -15,3 +30,24 @@ export default {
     }
 }
 </script>
+
+<style scoped lang="scss">
+.register {
+    text-align: center;
+
+    >h2 {
+        margin: 50px 0 30px 0;
+    }
+
+    .ui.form {
+        max-width: 300px !important;
+        margin: 0 auto;
+        margin-bottom: 10px;
+    }
+
+    input.error {
+        border-color: #faa;
+        background-color: #ffeded;
+    }
+}
+</style>
