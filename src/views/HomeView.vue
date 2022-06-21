0af383588e99ab951dd36efa6acfca84eb8b04cc
David Hernández <54561496+David-Hdez@users.noreply.github.com>
2022-06-20 21:13:58 -0500

    Obteniendo todos los productos
    API obtenido los ultimos productos
    registrados.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,11 +6,29 @@
 
 <script>
 import BasicLayout from '../layouts/BasicLayout'
+import { ref, onMounted } from 'vue'
 
 export default {
   name: 'Home',
   components: {
     BasicLayout
+  },
+  setup() {
+    let products = ref()
+
+    onMounted(async () => {
+      try {
+        const response = await fetch(`${process.env.VUE_APP_API_URL}/products?_sort=created_at:desc`);
+
+        const result = await response.json()
+        console.debug(result);
+        products.value = result;
+      } catch (error) {
+        console.error(error)
+      }
+    })
+
+    return { products }
   }
 }
 </script>
