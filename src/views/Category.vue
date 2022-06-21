45b5ad98afde8b9bf98ee7bb8fc5c7329e0c0f3f
David Hernández <54561496+David-Hdez@users.noreply.github.com>
2022-06-20 21:05:00 -0500

    Creando rutas dinamicas

---
+++ src/views/Category.vue
@@ -0,0 +1,25 @@
+<template>
+  <BasicLayout>
+    <h1>This is Category</h1>
+  </BasicLayout>
+</template>
+
+<script>
+import BasicLayout from '../layouts/BasicLayout'
+
+export default {
+  name: 'Category',
+  components: {
+    BasicLayout
+  },
+  created() {
+    this.$watch(
+      () => this.$route.params,
+      (toParams, previousParams) => {
+        // react to route changes...
+        console.debug(toParams)
+      }
+    )
+  },
+}
+</script>
