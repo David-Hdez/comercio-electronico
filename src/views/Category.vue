0abcbcfafb874117ecc0f6d40cb933e39c368f2c
David Hernández <54561496+David-Hdez@users.noreply.github.com>
2022-07-16 18:02:59 -0500

    Obteniendo productos al cambiar categoria
    Accessing to route for get the new param category.

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -5,35 +5,45 @@
 </template>
 
 <script>
-import { onMounted } from 'vue'
-import BasicLayout from '../layouts/BasicLayout'
+import { onMounted, watch, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import BasicLayout from '../layouts/BasicLayout';
 
 export default {
   name: 'Category',
   components: {
     BasicLayout
   },
-  created() {
-    this.$watch(
-      () => this.$route.params,
-      (toParams, previousParams) => {
-        // react to route changes...
-        console.debug('react to route changes', toParams)
+  setup() {
+    const route = useRoute();
+    let productsData = ref();
+
+    watch(
+      () => route.params.category,
+      async newCategory => {
+        products(newCategory);
       }
     )
-  },
-  setup() {
-    onMounted(async () => {
+
+    onMounted(() => {
+      products(route.params.category);
+    })
+
+    /**
+     * @see {@link https://router.vuejs.org/guide/advanced/composition-api.html#accessing-the-router-and-current-route-inside-setup}
+     * @see {@link https://router.vuejs.org/guide/essentials/dynamic-matching.html#reacting-to-params-changes}
+     */
+    const products = async (category) => {
       try {
-        const response = await fetch(`${process.env.VUE_APP_API_URL}/products?_where[category.slug]=cereales&_sort=created_at:desc`);
+        const response = await fetch(`${process.env.VUE_APP_API_URL}/products?_where[category.slug]=${category}&_sort=created_at:desc`);
 
-        const result = await response.json()
-
-        console.debug('by category', result);
+        const result = await response.json();
+        console.debug('products', result);
+        productsData.value = result;
       } catch (error) {
         console.error(error)
       }
-    })
+    }
   }
 }
 </script>
