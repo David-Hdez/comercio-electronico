32a4fd24f9c823629ecafd03ce69894701497ce1
David Hernández <54561496+David-Hdez@users.noreply.github.com>
2022-08-07 22:18:23 -0500

    Pintando pedidos
    Añadido moment.

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,6 +1,30 @@
 <template>
   <BasicLayout>
     <h1>Mis pedidos</h1>
+
+    <h3 v-if="!orders">No tienes pedidos</h3>
+
+    <template v-if="orders">
+      <div class="orders" v-for="order in orders" :key="order.id">
+        <div class="header-order">
+          <span class="date-order">{{ formatDate(order.created_at) }}</span>
+          <p>Orden {{ order.id }}</p>
+          <p>Total ${{ order.totalPayment }}</p>
+        </div>
+
+        <div class="products">
+          <div class="product" v-for="product in order.detail" :key="product.id">
+            <div>
+              <img :src="api_url + product.logo.url" :alt="product.name" class="ui image">
+              <p>{{ product.name }}</p>
+            </div>
+            <div>
+              {{ product.quantity }} x ${{ product.price }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
   </BasicLayout>
 </template>
 
@@ -9,12 +33,14 @@ import { ref, onMounted } from 'vue';
 import BasicLayout from '../layouts/BasicLayout';
 import jwt_decode from "jwt-decode";
 import Cookies from 'js-cookie';
+import moment from 'moment';
 
 export default {
   name: 'Orders',
   components: { BasicLayout },
   setup() {
     const orders = ref();
+    const api_url = process.env.VUE_APP_API_URL;
 
     onMounted(async () => {
       const jwt = Cookies.get('jwt');
@@ -31,14 +57,61 @@ export default {
       }
     })
 
-    return { orders }
+    const formatDate = (order) => {
+      return moment(order).format('L [a las] LT')
+    }
+
+    return { orders, formatDate, api_url }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.product-action {
-  text-align: center;
-  cursor: pointer;
+.orders {
+  border: 1px solid #000;
+  margin-bottom: 20px;
+  margin-top: 40px;
+
+  .header-order {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px;
+
+    p {
+      margin: 0;
+    }
+
+    span {
+      position: absolute;
+      top: -20px;
+      left: 0;
+      font-size: 12px;
+    }
+  }
+
+  .products {
+    border-top: 1px solid #000;
+
+    .product {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 5px 30px;
+
+      /* div at the first level */
+      >div {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      }
+    }
+
+    .ui.image {
+      width: 50px;
+      padding: 2px;
+    }
+  }
 }
 </style>
